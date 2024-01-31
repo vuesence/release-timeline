abbc4f854e2745c20d8e270bb7f98f00f013fa05
altrusl <ruslan.makarov@gmail.com>
2024-01-31 08:33:40 +0000

    Deploying to gh-pages from @ vuesence/release-timeline@c0e12f4b695a795405b99256b3043f5323929093 🚀

--- date-utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function isSameDay(date1: Date, date2: Date): boolean;
-export declare function eachDayOfInterval(startDate: Date, endDate: Date): Date[];
--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import ReleaseTimeline from "./ReleaseTimeline.vue";
-import { DefaultOptions } from "./options";
-/** Types */
-/** Components */
-export { ReleaseTimeline, DefaultOptions };
-export {};
