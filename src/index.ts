515c9292af5409f6959406103a45bbcb79df2ea1
Ruslan Makarov <ruslan.makarov@gmail.com>
2024-02-02 14:09:36 +0300

    fix: custom element in VitePress issue

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import ReleaseTimelineCE from "./ReleaseTimeline.ce.vue";
 import ReleaseTimeline from "./ReleaseTimeline.vue";
 import { DefaultOptions } from "./options";
 
-export { ReleaseTimeline, DefaultOptions };
+// function initCE() {
+window.customElements.define("release-timeline", defineCustomElement(ReleaseTimelineCE));
+// }
 
-customElements.define("release-timeline", defineCustomElement(ReleaseTimelineCE));
+export { ReleaseTimeline, DefaultOptions };
 
 export {};
